5463942997d57f1cf6cd611fdfb1b11fe6aed167
Aditya <114802996+AdityaKalmekh@users.noreply.github.com>
2025-05-31 20:11:14 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/(.*)',
+        destination: 'https://chat-frontend-ivory-rho.vercel.app/',
+        permanent: true,
+      },
+    ]
+  },
+};
 
 export default nextConfig;
